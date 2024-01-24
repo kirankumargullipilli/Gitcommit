8f54728952c3ee7bde5fcbe0184f557b93f6574e
kirankumargullipilli <kirankumargullipilli22@gmail.com>
2024-01-24 15:28:25 -0600

    First GitCommit

---
+++ Helloo/src/helloo/Helloo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloo;
+
+/**
+ *
+ * @author S572285
+ */
+public class Helloo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
